3d9d220ae4885d1c171288053f799de88619f6e8
Dwyrain <dwyrain@rambler.ru>
2019-03-17 16:28:38 +0400

    add test for DefaultModule

---
+++ tests/DefaultModuleTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use kirillGru\textSearcher\modules\DefaultModule;
+
+class DefaultModuleTest extends \Codeception\Test\Unit
+{
+    private $queries;
+    private $file_data;
+
+    protected function _before()
+    {
+        $this->queries = [
+            // query_string , expected: false or [line, column]
+            ['Hello world!', [2,1]],
+            ['Orange not is fruit', false]
+        ];
+        $this->file_data = "Something\r\nHello world!\r\nLorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam architecto, atque blanditiis consectetur corporis dolor esse excepturi harum ipsa laudantium, molestiae numquam omnis repellendus totam! Assumenda officiis sequi veniam.";
+
+    }
+
+    // tests
+    public function testSearch()
+    {
+        $fileSource = $this->mockFileSource();
+        $module = new DefaultModule();
+        foreach ($this->queries as $query) {
+            $expected = $query[1];
+            if(is_array($expected)){
+                $result = $module->search($fileSource, $query[0]);
+                $this->assertArrayHasKey('line', $result, 'empty result');
+                $this->assertArrayHasKey('column', $result, 'empty result');
+                $this->assertTrue($expected['line'] === $result['line'], 'Incorrect line matches');
+                $this->assertTrue($expected['column'] === $result['column'], 'Incorrect column matches');
+            }else{
+                $this->assertEquals($query[1], $module->search($fileSource, $query[0]));
+            }
+        }
+    }
+
+    private function mockFileSource()
+    {
+        return $this->make('\\kirillGru\\textSearcher\\fileSources\\LocalFile', [
+            'readLine' => function () {
+                static $end = false;
+                if($end === true){
+                    return null;
+                }else{
+                    $end  = true;
+                    return $this->file_data;
+                }
+            }
+        ]);
+    }
+}
